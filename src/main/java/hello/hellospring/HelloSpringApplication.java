bce7d9bd78bf086bcb5393318009ecfc630018a1
moon <robort123@naver.com>
2022-12-04 03:21:22 +0900

    [Feat] Current Code Save

--- src/main/java/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -7,13 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloSpringApplication {
 
     public static void main(String[] args) {
-        for(int i=0;i<5;i++){
-            System.out.println("Hello World");
-        }
-
-
-
-        //SpringApplication.run(HelloSpringApplication.class, args);
+        SpringApplication.run(HelloSpringApplication.class, args);
     }
 
 }
